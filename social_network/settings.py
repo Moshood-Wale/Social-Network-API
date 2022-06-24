c8dcdf542226d5087cf6b3fe1d4183f40fb1c429
Moshood Salawudeen <86296143+Moshood-Wale@users.noreply.github.com>
2022-06-24 18:50:51 +0100

    Update settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -12,7 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-twkm#*rhrjsgm!bk4zr-xj!o$0x3cwdfbku$m6r+j%_d3z$h5m'
 SECRET_KEY = config("SECRET_KEY", default=get_random_secret_key(), cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
